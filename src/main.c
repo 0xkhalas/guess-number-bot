1d616c4cb72f4f53848f19d990b1fb980f8a8b9e
Ali Ayoub <77502484+0xkhalas@users.noreply.github.com>
2024-02-27 05:54:24 +0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,12 +6,6 @@
 #define BOT_TOKEN ""
 #define GUILD_ID 939501463825170472
 
-
-typedef struct {
-	unsigned long long user_id;
-	int random_number;
-} PlayerInteraction;
-
 void on_ready(struct discord *client, const struct discord_ready *event) {
 	for (int i = 0; i < sizeof(globalCommands) / sizeof(SlashCommands); i++) {
 		struct discord_create_guild_application_command params = {
@@ -33,10 +27,20 @@ void on_interaction(struct discord *client, const struct discord_interaction *ev
 	}
 }
  
+void on_message_create(struct discord *client, const struct discord_message *event) {
+	if (event->author == NULL) return;
+    if (event->author->bot) return;
+ 
+	guess_result(client, event);
+}
+
 int main(void) {
     struct discord *client = discord_init(BOT_TOKEN);
 	init_commands();
+	discord_add_intents(client, DISCORD_GATEWAY_MESSAGE_CONTENT);
     discord_set_on_ready(client, &on_ready);
+	discord_set_on_message_create(client, on_message_create);
     discord_set_on_interaction_create(client, &on_interaction);
     discord_run(client);
+	free_commands();
 }
